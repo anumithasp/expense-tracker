22d76e4d455588793616af33c1df57166fd1f156
Abinithya <abinithya.samdas@gmail.com>
2024-03-13 11:33:54 +0530

    signup initial changes

--- src/components/FeatureBanner/FeatureBanner.jsx
+++ src/components/FeatureBanner/FeatureBanner.jsx
@@ -3,24 +3,24 @@ import './FeatureBanner.css'
 
 const FeatureBanner = (props) => {
     const leftAlignCode = <div className='row align-items-center bg-light bg-gradient feature-banner left'>
-                            <div className='col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1'></div>
-                            <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3'>
+                            <div className='col col-12 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1'></div>
+                            <div className='col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3'>
                                 <img src={props.image} alt="Safe" style={{height: 160}}/>
                             </div>
-                            <div className='col-7 col-sm-7 col-md-7 col-lg-7 col-xl-7 col-xxl-7'>
+                            <div className='col col-12 col-sm-7 col-md-7 col-lg-7 col-xl-7 col-xxl-7'>
                                 <h3>{props.words[0]}<b>{props.words[1]}</b>{props.words[2]}</h3>
                             </div>
-                            <div className='col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1'></div>
+                            <div className='col col-12 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1'></div>
                         </div>
     const rightAlignCode = <div className='row align-items-center bg-light bg-gradient feature-banner right'>
-                                <div className='col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1'></div>
-                                <div className='col-7 col-sm-7 col-md-7 col-lg-7 col-xl-7 col-xxl-7 px-5'>
+                                <div className='col col-12 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1'></div>
+                                <div className='col col-12 col-sm-7 col-md-7 col-lg-7 col-xl-7 col-xxl-7 px-5'>
                                     <h3>{props.words[0]}<b>{props.words[1]}</b>{props.words[2]}</h3>
                                 </div>
-                                <div className='col-3 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3'>
+                                <div className='col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3'>
                                     <img src={props.image} alt="Safe" style={{height: 160}}/>
                                 </div>
-                                <div className='col-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1'></div>
+                                <div className='col col-12 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-xxl-1'></div>
                             </div>
 
     let renderCode;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material'
 import React from 'react'
+import './Services.css'
 
 const Services = () => {
     return (
@@ -7,56 +8,61 @@ const Services = () => {
             <div className='row justify-content-center align-items-center feature-title'>
                 <h2>Our Services</h2>
             </div>
-            <div className='d-flex justify-content-between'>
-                <Box className='service-box'
-                    height={200}
-                    width={400}
-                    my={4}
-                    borderRadius={2}
-                    display="flex"
-                    alignItems="center"
-                    gap={4}
-                    p={2}
-                    sx={{ marginTop: '0', background: 'linear-gradient(0.75turn, #972732, 50%, #e08c95)', color: 'white'}}
-                    >
-                    <div>
-                        <h2>Budgeting</h2>
-                        <h5 style={{lineHeight: '1.5', fontWeight: '300', fontSize: 'large'}}>Say goodbye to guesswork and hello to precision with our intuitive budgeting tools.</h5>
-                    </div>
-                    
-                </Box>
-                <Box
-                    height={200}
-                    width={400}
-                    my={4}
-                    borderRadius={2}
-                    display="flex"
-                    alignItems="center"
-                    gap={4}
-                    p={2}
-                    sx={{ marginTop: '0', background: 'linear-gradient(0.25turn, #006647, 50%, #00b57e)', color: 'white' }}
-                    >
-                    <div>
-                        <h2>Setting Goals</h2>
-                        <h5 style={{lineHeight: '1.5', fontWeight: '300', fontSize: 'large'}}>With our goal-setting feature, setting and achieving financial goals is at the heart of what we do.</h5>
-                    </div>
-                </Box>
-                <Box
-                    height={200}
-                    width={400}
-                    my={4}
-                    borderRadius={2}
-                    display="flex"
-                    alignItems="center"
-                    gap={4}
-                    p={2}
-                    sx={{ marginTop: '0', background: 'linear-gradient(0.75turn, #014983, 50%, #558eba)', color: 'white' }}
-                    >
-                    <div>
-                        <h2>Compare & Grow</h2>
-                        <h5 style={{lineHeight: '1.5', fontWeight: '300', fontSize: 'large'}}>With our intuitive comparision feature, you can easily visualize and analyze your financial inflows and outflows.</h5>
-                    </div>
-                </Box>
+            <div className='boxes row justify-content-between'>
+                <div className='col col-12 col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4'>
+                    <Box className='service-box'
+                        height={200}
+                        width={400}
+                        my={4}
+                        borderRadius={2}
+                        display="flex"
+                        alignItems="center"
+                        gap={4}
+                        p={2}
+                        sx={{ marginTop: '0', background: 'linear-gradient(0.75turn, #972732, 50%, #e08c95)', color: 'white'}}
+                        >
+                        <div>
+                            <h2>Budgeting</h2>
+                            <h5 style={{lineHeight: '1.5', fontWeight: '300', fontSize: 'large'}}>Say goodbye to guesswork and hello to precision with our intuitive budgeting tools.</h5>
+                        </div>   
+                    </Box>
+                </div>
+                <div className='col col-12 col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4'>
+                    <Box className='service-box'
+                        height={200}
+                        width={400}
+                        my={4}
+                        borderRadius={2}
+                        display="flex"
+                        alignItems="center"
+                        gap={4}
+                        p={2}
+                        sx={{ marginTop: '0', background: 'linear-gradient(0.25turn, #006647, 50%, #00b57e)', color: 'white' }}
+                        >
+                        <div>
+                            <h2>Setting Goals</h2>
+                            <h5 style={{lineHeight: '1.5', fontWeight: '300', fontSize: 'large'}}>With our goal-setting feature, setting and achieving financial goals is at the heart of what we do.</h5>
+                        </div>
+                    </Box>
+                </div>
+                <div className='col col-12 col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4'>
+                    <Box className='service-box'
+                        height={200}
+                        width={400}
+                        my={4}
+                        borderRadius={2}
+                        display="flex"
+                        alignItems="center"
+                        gap={4}
+                        p={2}
+                        sx={{ marginTop: '0', background: 'linear-gradient(0.75turn, #014983, 50%, #558eba)', color: 'white' }}
+                        >
+                        <div>
+                            <h2>Compare & Grow</h2>
+                            <h5 style={{lineHeight: '1.5', fontWeight: '300', fontSize: 'large'}}>With our intuitive comparision feature, you can easily visualize and analyze your financial inflows and outflows.</h5>
+                        </div>
+                    </Box>
+                </div> 
             </div>
         </div>
     )
