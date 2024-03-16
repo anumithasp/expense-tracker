a7c752a15a52ff3cf54bfa4c5e36371d99644f7e
Abinithya <abinithya.samdas@gmail.com>
2024-03-16 12:02:16 +0530

    toast and sign up validation update

---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Toast.css';
+
+const Toast = (props) => {
+    return (
+      <div className="toast-container position-fixed top-0 end-0 p-3">
+          <div id="liveToast" className="toast exp-toast" role="alert" aria-live="assertive" aria-atomic="true">
+            <div className='d-flex'>
+              <div className="toast-body toast-msg">
+                {props.message}
+              </div>
+              <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+          </div>
+      </div>
+    )
+  }
+
+export default Toast
