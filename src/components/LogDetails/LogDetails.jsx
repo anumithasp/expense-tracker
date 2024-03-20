7af4e26b6aa3a2373fba3ab09b799e3f0e8dfe0d
Abinithya <abinithya.samdas@gmail.com>
2024-03-20 10:46:30 +0530

    dashboard changes

--- src/components/LogDetails/LogDetails.jsx
+++ src/components/LogDetails/LogDetails.jsx
@@ -2,17 +2,60 @@ import React from 'react';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import './LogDetails.css';
+import { CircularProgress, Divider, Grid, IconButton, LinearProgress, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     textAlign: 'center',
     color: theme.palette.text.secondary,
-    height: 175,
+    height: 500,
     width : 450,
     lineHeight: '60px'
   }));
 
 const lightTheme = createTheme({ palette: { mode: 'light' } });
+
+const dataSet = [
+    {
+        name: "Shopping",
+        amount: 3000,
+    },
+    {
+        name: "Food",
+        amount: 1500,
+    },
+    {
+        name: "Education",
+        amount: 4000,
+    },
+    {
+        name: "Vacation",
+        amount: 10000,
+    },
+    {
+        name: "Grocery",
+        amount: 3000,
+    },
+    {
+        name: "Transportation",
+        amount: 1000,
+    },
+    {
+        name: "Insurance",
+        amount: 2250,
+    },
+    {
+        name: "Gifts",
+        amount: 500,
+    },
+    {
+        name: "Others",
+        amount: 750,
+    }
+];
+
+
 
 const LogDetails = () => {
   return (
@@ -28,13 +71,28 @@ const LogDetails = () => {
             >
                 <Item key={1} elevation={1}>
                     <div className='transaction'>
-                        <div className='header px-3'>
-                            <h5><b>REMAINING BALANCE</b></h5>
-                            <a href='#'>
-                            See details
-                            </a>
+                        <div className='transaction-details px-3'>
+                            <h5>Latest Transactions</h5>
                         </div>
                     </div>
+                    <Grid item xs={12} md={6}>
+                        <List>
+                        {dataSet.map((category) => (
+                            <div>
+                                <ListItem
+                                    secondaryAction={category.amount}
+                                    >
+                                    <CircularProgress sx={{ padding: '5px' }} size="2rem" variant="determinate" value={100} />
+                                    <ListItemText
+                                        primary={category.name}
+                                    />
+                                    <LinearProgress color="primary" variant="buffer" value={50} />
+                                </ListItem>
+                                <Divider variant="middle" component="li" />
+                            </div>
+                        ))}
+                        </List>
+                    </Grid>
                 </Item>
             </Box>
         </ThemeProvider>
